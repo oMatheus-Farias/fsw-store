ddec7c2c150a0ee7a9ed601e21dd1dab765e0b72
Matheus <matheusfariasdasneves@gmail.com>
2024-03-26 17:49:55 -0300

    feat: add stripe webhook

---
+++ src/app/api/order/payment-success/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+});
+
+export const POST = async (req: Request) => {
+  const signature = req.headers.get("stripe-signature");
+
+  if (!signature) {
+    return NextResponse.error();
+  }
+
+  const text = await req.text();
+
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET_KEY,
+  );
+
+  if (event.type === "checkout.session.completed") {
+    const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+      event.data.object.id,
+      {
+        expand: ["line_items"],
+      },
+    );
+    const lineItems = sessionWithLineItems.line_items;
+
+    console.log(lineItems);
+  }
+
+  return NextResponse.json({ received: true });
+};
