fbe663461f57ea5dae66fe0bdd76f555f26277d3
Matheus <matheusfariasdasneves@gmail.com>
2024-03-28 21:50:42 -0300

    feat: add desktop banner to home page

--- src/app/(home)/components/promo-banner.tsx
+++ src/app/(home)/components/promo-banner.tsx
@@ -1,13 +1,14 @@
+import { cn } from "@/lib/utils";
 import { ImageProps } from "next/image";
 import Image from "next/image";
 
-const PromoBanner = ({ alt, ...props }: ImageProps) => {
+const PromoBanner = ({ alt, className, ...props }: ImageProps) => {
   return (
     <Image
       width={0}
       height={0}
       sizes="100vw"
-      className="h-auto w-full p-5"
+      className={cn("h-auto w-full p-5", className)}
       priority
       alt={alt}
       {...props}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -31,41 +31,57 @@ const Home = async () => {
   });
 
   return (
-    <div className="flex flex-col gap-8 py-8">
-      <PromoBanner
-        src="/banner-home-01.png"
-        alt="Até 55% de desconto só esse mês"
-      />
-
-      <div className="p-5">
-        <Categories />
+    <>
+      <div className="mx-auto max-w-[1920px]">
+        <Image
+          src="/deals-banner.png"
+          alt="Até 55% de desconto só esse mês"
+          width={0}
+          height={0}
+          sizes="100vw"
+          priority
+          className="hidden h-auto w-full lg:block"
+        />
       </div>
 
-      <div>
-        <SectionTitle>Ofertas</SectionTitle>
-        <ProductList products={deals} />
+      <div className="container flex flex-col gap-8 py-8">
+        <PromoBanner
+          src="/banner-home-01.png"
+          alt="Até 55% de desconto só esse mês"
+          priority
+          className="lg:hidden"
+        />
+
+        <div className="p-5">
+          <Categories />
+        </div>
+
+        <div>
+          <SectionTitle>Ofertas</SectionTitle>
+          <ProductList products={deals} />
+        </div>
+
+        <PromoBanner
+          src="/banner-home-02.png"
+          alt="Até 55% de desconto em mouses"
+        />
+
+        <div>
+          <SectionTitle>Teclados</SectionTitle>
+          <ProductList products={keyboards} />
+        </div>
+
+        <PromoBanner
+          src="/banner-home-03.png"
+          alt="Até 20% de desconto em fones"
+        />
+
+        <div>
+          <SectionTitle>Mouses</SectionTitle>
+          <ProductList products={mouses} />
+        </div>
       </div>
-
-      <PromoBanner
-        src="/banner-home-02.png"
-        alt="Até 55% de desconto em mouses"
-      />
-
-      <div>
-        <SectionTitle>Teclados</SectionTitle>
-        <ProductList products={keyboards} />
-      </div>
-
-      <PromoBanner
-        src="/banner-home-03.png"
-        alt="Até 20% de desconto em fones"
-      />
-
-      <div>
-        <SectionTitle>Mouses</SectionTitle>
-        <ProductList products={mouses} />
-      </div>
-    </div>
+    </>
   );
 };
 
