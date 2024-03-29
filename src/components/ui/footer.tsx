29e3052a6960d9edacd948bc89ff607d13bf526a
Matheus <matheusfariasdasneves@gmail.com>
2024-03-28 22:53:16 -0300

    feat: ensure footer is responsive on desktop

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-accent px-8 py-4 ">
-      <p className="text-[0.62em] opacity-75">
+      <p className="text-[0.62em] opacity-75 lg:container lg:text-sm">
         © 2023 Copyright <span className="font-semibold">FSW Store</span>
       </p>
     </footer>
