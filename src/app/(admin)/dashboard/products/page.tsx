84e6f8d8cf22fcaa921febe85ed64edbb744e2a0
Matheus <matheusfariasdasneves@gmail.com>
2024-03-29 23:48:27 -0300

    feat: ensure add button and products found quantity are shown in products page

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -1,4 +1,7 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { prismaClient } from "@/lib/prisma";
+import { PackageIcon, PlusIcon } from "lucide-react";
 
 const ProductsPage = async () => {
   const products = await prismaClient.product.findMany({});
@@ -6,8 +9,22 @@ const ProductsPage = async () => {
   console.log(products);
 
   return (
-    <div>
-      <h1>products</h1>
+    <div className="flex w-full flex-col gap-10 p-10">
+      <Badge variant="heading">
+        <PackageIcon size={18} />
+        Produtos
+      </Badge>
+
+      <div className="flex w-full items-center justify-between">
+        <p className="text-lg font-bold">
+          Produtos encontrados: {products.length}
+        </p>
+
+        <Button className="flex items-center gap-2 font-bold">
+          <PlusIcon size={16} />
+          Adicionar produto
+        </Button>
+      </div>
     </div>
   );
 };
