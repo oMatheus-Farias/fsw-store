3896ac635781bf71e78c11399c02e60c72cb3106
Matheus <matheusfariasdasneves@gmail.com>
2024-03-29 23:33:29 -0300

    feat: ensure dashboard and products button have correct background on selected

--- src/app/(admin)/dashboard/components/sidebar.tsx
+++ src/app/(admin)/dashboard/components/sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   LayoutDashboardIcon,
@@ -5,8 +7,12 @@ import {
   PackageIcon,
   PackageSearchIcon,
 } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const path = usePathname();
+
   return (
     <div className="flex min-w-[300px] flex-col items-center gap-8 border-r border-solid border-accent bg-background p-8">
       <h1 className="text-lg font-semibold">
@@ -14,22 +20,46 @@ const Sidebar = () => {
       </h1>
 
       <div className="flex w-full flex-col gap-3">
-        <Button variant="outline" className="flex w-full justify-start gap-2">
-          <LayoutDashboardIcon size={16} />
-          Dashboard
-        </Button>
+        <Link href="/dashboard">
+          <Button
+            variant="outline"
+            className={`flex w-full justify-start gap-2
+          ${path === "/dashboard" && "bg-primary text-white hover:bg-primary"}
+          `}
+          >
+            <LayoutDashboardIcon size={16} />
+            Dashboard
+          </Button>
+        </Link>
 
-        <Button variant="outline" className="flex w-full justify-start gap-2">
-          <PackageIcon size={16} />
-          Produtos
-        </Button>
+        <Link href="/dashboard/products">
+          <Button
+            variant="outline"
+            className={`flex w-full justify-start gap-2
+          ${path.includes("/products") && "bg-primary text-white hover:bg-primary"}
+          `}
+          >
+            <PackageIcon size={16} />
+            Produtos
+          </Button>
+        </Link>
 
-        <Button variant="outline" className="flex w-full justify-start gap-2">
+        <Button
+          variant="outline"
+          className={`flex w-full justify-start gap-2
+          ${path.includes("/categories") && "bg-primary text-white hover:bg-primary"}
+          `}
+        >
           <ListOrderedIcon size={16} />
           Categorias
         </Button>
 
-        <Button variant="outline" className="flex w-full justify-start gap-2">
+        <Button
+          variant="outline"
+          className={`flex w-full justify-start gap-2
+          ${path.includes("/orders") && "bg-primary text-white hover:bg-primary"}
+          `}
+        >
           <PackageSearchIcon size={16} />
           Pedidos
         </Button>
---
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -0,0 +1,9 @@
+const ProductsPage = () => {
+  return (
+    <div>
+      <h1>products</h1>
+    </div>
+  );
+};
+
+export default ProductsPage;
