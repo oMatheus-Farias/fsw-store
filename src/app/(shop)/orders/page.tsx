5ac1b5dddf8d43811f2d1de9a62b4b981d54bf6f
Matheus <matheusfariasdasneves@gmail.com>
2024-03-30 17:00:46 -0300

    feat: add orders page to dashboard

--- src/app/(shop)/orders/page.tsx
+++ src/app/(shop)/orders/page.tsx
@@ -3,7 +3,7 @@ import { authOptions } from "@/lib/auth";
 import { prismaClient } from "@/lib/prisma";
 import { PackageSearchIcon } from "lucide-react";
 import { getServerSession } from "next-auth";
-import OrderItem from "./components/order-item";
+import OrderItem from "../../../components/ui/order-item";
 
 const OrdersPage = async () => {
   const user = await getServerSession(authOptions);
