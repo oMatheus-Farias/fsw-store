7603fc0ed71992049c5a3e69f7ba4b23b7f92566
Matheus <matheusfariasdasneves@gmail.com>
2024-03-30 02:31:24 -0300

    feat: add initial products table component

--- src/app/(admin)/dashboard/products/page.tsx
+++ src/app/(admin)/dashboard/products/page.tsx
@@ -2,11 +2,15 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { prismaClient } from "@/lib/prisma";
 import { PackageIcon, PlusIcon } from "lucide-react";
+import ProductsTable from "./components/products-table";
+import { computeProductTotalPrice } from "@/helpers/product";
 
 const ProductsPage = async () => {
   const products = await prismaClient.product.findMany({});
 
-  console.log(products);
+  const productsWithTotalPrice = products.map((product) =>
+    computeProductTotalPrice(product),
+  );
 
   return (
     <div className="flex w-full flex-col gap-10 p-10">
@@ -25,6 +29,8 @@ const ProductsPage = async () => {
           Adicionar produto
         </Button>
       </div>
+
+      <ProductsTable products={productsWithTotalPrice} />
     </div>
   );
 };
