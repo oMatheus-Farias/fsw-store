da3e6b533c4ce5af0b92d9dafb4f12d96da00964
Matheus <matheusfariasdasneves@gmail.com>
2024-04-01 16:20:14 -0300

    fix: ensure product list shown correctly on home page desktop

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -8,7 +8,7 @@ interface ProductListProps {
 
 const ProductList = ({ products }: ProductListProps) => {
   return (
-    <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+    <div className="flex w-full gap-4 overflow-x-auto px-5 lg:grid lg:grid-cols-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
         <div
           key={product.id}
